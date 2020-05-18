4889ee8252cd28f32f43f88eaf3912e41e3bb2a9
JPCatarino <jorge.catarino@ua.pt>
2020-05-18 18:19:09 +0100

    [GEN] Add simple generator code

---
+++ Classes/UserGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ihcProject.Classes
+{
+    class UserGenerator
+    {
+        UserTemplate userTemp;
+        public UserGenerator(String username, String password) {
+            userTemp = new UserTemplate();
+            userTemp.username = username;
+            userTemp.password = password;
+        }
+
+        public void generate() {
+            userTemp.avatar_url = randomAvatar();
+            userTemp.statistics = generateStatistics();
+        }
+
+        // Grabs a random avatar from the assets and returns it
+        private String randomAvatar() {
+            var rand = new Random();
+            var files = Directory.GetFiles("../assets/images/avatars", "*.png");
+            return files[rand.Next(files.Length)];
+        }
+
+        private Statistics generateStatistics() {
+            Statistics ret = new Statistics();
+            var rand = new Random();
+
+            // Generate Level
+            ret.level = new Level();
+            ret.level.current = rand.Next(100);
+
+            // Generate pp
+            ret.pp = rand.Next(19321);
+
+            // Generate Rank
+            // TODO make this aware of other users.
+            ret.rank = new Rank();
+            ret.rank.global = rand.Next(1000);
+            ret.rank.country = rand.Next((int)ret.rank.global);
+
+            return ret;
+        }
+    }
+}
---
+++ Classes/UserTemplate.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ihcProject.Classes
+{
+    public class UserTemplate
+    {
+        public string avatar_url { get; set; }
+        public string country_code { get; set; }
+        public string default_group { get; set; }
+        public int id { get; set; }
+        public bool is_active { get; set; }
+        public bool is_bot { get; set; }
+        public bool is_online { get; set; }
+        public bool is_supporter { get; set; }
+        public DateTime last_visit { get; set; }
+        public bool pm_friends_only { get; set; }
+        public object profile_colour { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string cover_url { get; set; }
+        public object discord { get; set; }
+        public bool has_supported { get; set; }
+        public object interests { get; set; }
+        public DateTime join_date { get; set; }
+        public Kudosu kudosu { get; set; }
+        public object lastfm { get; set; }
+        public object location { get; set; }
+        public int max_blocks { get; set; }
+        public int max_friends { get; set; }
+        public object occupation { get; set; }
+        public string playmode { get; set; }
+        public string[] playstyle { get; set; }
+        public int post_count { get; set; }
+        public string[] profile_order { get; set; }
+        public object skype { get; set; }
+        public object title { get; set; }
+        public object twitter { get; set; }
+        public object website { get; set; }
+        public Country country { get; set; }
+        public Cover cover { get; set; }
+        public object[] account_history { get; set; }
+        public object[] active_tournament_banner { get; set; }
+        public object[] badges { get; set; }
+        public int favourite_beatmapset_count { get; set; }
+        public int follower_count { get; set; }
+        public int graveyard_beatmapset_count { get; set; }
+        public int loved_beatmapset_count { get; set; }
+        public Monthly_Playcounts[] monthly_playcounts { get; set; }
+        public Page page { get; set; }
+        public object[] previous_usernames { get; set; }
+        public int ranked_and_approved_beatmapset_count { get; set; }
+        public object[] replays_watched_counts { get; set; }
+        public int scores_first_count { get; set; }
+        public Statistics statistics { get; set; }
+        public int support_level { get; set; }
+        public int unranked_beatmapset_count { get; set; }
+        public User_Achievements[] user_achievements { get; set; }
+        public object rankHistory { get; set; }
+    }
+
+    public class Kudosu
+    {
+        public int total { get; set; }
+        public int available { get; set; }
+    }
+
+    public class Country
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Cover
+    {
+        public object custom_url { get; set; }
+        public string url { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Page
+    {
+        public string html { get; set; }
+        public string raw { get; set; }
+    }
+
+    public class Statistics
+    {
+        public Level level { get; set; }
+        public int pp { get; set; }
+        public int pp_rank { get; set; }
+        public int ranked_score { get; set; }
+        public int hit_accuracy { get; set; }
+        public int play_count { get; set; }
+        public int play_time { get; set; }
+        public int total_score { get; set; }
+        public int total_hits { get; set; }
+        public int maximum_combo { get; set; }
+        public int replays_watched_by_others { get; set; }
+        public bool is_ranked { get; set; }
+        public Grade_Counts grade_counts { get; set; }
+        public Rank rank { get; set; }
+    }
+
+    public class Level
+    {
+        public int current { get; set; }
+        public int progress { get; set; }
+    }
+
+    public class Grade_Counts
+    {
+        public int ss { get; set; }
+        public int ssh { get; set; }
+        public int s { get; set; }
+        public int sh { get; set; }
+        public int a { get; set; }
+    }
+
+    public class Rank
+    {
+        public object global { get; set; }
+        public object country { get; set; }
+    }
+
+    public class Monthly_Playcounts
+    {
+        public string start_date { get; set; }
+        public int count { get; set; }
+    }
+
+    public class User_Achievements
+    {
+        public DateTime achieved_at { get; set; }
+        public int achievement_id { get; set; }
+    }
+
+}
