1bbe593462a79360d1fd23ba911dc1958e06b09d
JPCatarino <jorge.catarino@ua.pt>
2020-05-25 19:08:29 +0100

    [BMR] Adding randomizing

--- Data/staticBeatmapRecData.cs
+++ Data/staticBeatmapRecData.cs
@@ -10,32 +10,49 @@ namespace ihcProject.Data
     {
         public Uri StaticRankIcon
         {
-            get { return new Uri("pack://application:,,,/assets/images/icons/ranks/ranking-S.png"); }
+            get {
+                string[] rank = { "A", "B", "C", "D", "S", "SH", "X", "XH" };
+                Random random = new Random();
+                string r = rank[random.Next(0, 8)];
+                return new Uri("pack://application:,,,/assets/images/icons/ranks/ranking-"+ r + ".png"); }
         }
 
         public Uri StaticBeatmapImage
         {
-            get { return new Uri("pack://application:,,,/assets/images/beatmaps/4.jpg"); }
+            get {
+                Random random = new Random();
+                return new Uri("pack://application:,,,/assets/images/beatmaps/"+ random.Next(1,11).ToString() +".jpg"); }
         }
 
         public String StaticBeatmapName
         {
-            get { return "Test Beatmap"; }
+            get {
+                Random random = new Random();
+                string[] btn = { "Rubber Band", "anticrystal", "Akari no Arika", "WANNABE", "Mare Maris", "Dramatic Star", "Dream Catcher", "eastward", "CHAIN", "veil", "A Garagem da Vizinha" };
+                return btn[random.Next(0,11)]; }
         }
 
         public Uri StaticGameModeIcon
         {
-            get { return new Uri("pack://application:,,,/assets/images/icons/game_modes/mode-taiko.png"); }
+            get {
+                Random random = new Random();
+                string[] md = { "osu", "mania", "fruits", "taiko"};
+                return new Uri("pack://application:,,,/assets/images/icons/game_modes/mode-" + md[random.Next(0,4)]  + ".png"); }
         }
 
         public int StaticBeatmapPercentage
         {
-            get { return 98; }
+            get {
+                Random random = new Random();
+                return random.Next(80,101); }
         }
 
         public int StaticBeatmapPP
         {
-            get { return 132; }
+            get {
+                Random random = new Random();
+                return random.Next(50, 400);
+            }
         }
     }
 
