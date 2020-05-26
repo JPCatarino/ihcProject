33fcf9b39987c27ce3236962fbf5fdc54c03424b
ProudBloom <jakub-sztompka@wp.pl>
2020-05-26 13:39:48 +0200

    stat page finished

--- Pages/UserStatsPage.xaml.cs
+++ Pages/UserStatsPage.xaml.cs
@@ -23,11 +23,7 @@ namespace ihcProject.Pages
         public UserTemplate cUserData
         { get; set; }
 
-        public SeriesCollection seriesCollection { get; set; }
-        public SeriesCollection seriesCollection2 { get; set; }
-        public SeriesCollection seriesCollection3 { get; set; }
-        public string[] labelsX { get; set; }
-        public string[] labelsRankY { get; set; }
+
 
         private void pieChart_dataClick(object sender, ChartPoint chartPoint)
         {
@@ -42,43 +38,7 @@ namespace ihcProject.Pages
             series.PushOut = 8;
         }
 
-        public void cartesianChart()
-        {
-            seriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "PP",
-                    Values = new ChartValues<double>{8254, 8912, 9510, 9984, 10116, 10598, 10920, 11034, 11473, 11927, 12142, 12448},
-                    PointGeometry=DefaultGeometries.Circle,
-                }
-            };
-
-            seriesCollection2 = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Rank",
-                    Values = new ChartValues<double>{1182, 1021, 1094, 953, 724, 904, 742, 701, 689, 634, 611, 598},
-                    PointGeometry=DefaultGeometries.Circle,
-                    Stroke = Brushes.Red,
-                },
-            };
-
-            seriesCollection3 = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Plays",
-                    Values = new ChartValues<double>{1452, 1261, 3126, 3162, 2156, 1351, 2909, 1003, 927, 589, 1250, 3156},
-                    PointGeometry=DefaultGeometries.Square,
-                    Stroke = Brushes.Green,
-                },
-            };
-
-            labelsX = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-            labelsRankY = new string[] {"1200", "1100", "1000", "900", "800", "700", "600", "500" };
-        }
+        
 
         public UserStats()
         {
@@ -86,7 +46,6 @@ namespace ihcProject.Pages
             cUserData = PlayerWindow.cUserData;
             DataContext = cUserData;
 
-            this.cartesianChart();
             InitializeComponent();
         }
 
